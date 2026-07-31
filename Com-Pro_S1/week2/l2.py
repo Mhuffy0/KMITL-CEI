6de7adc2a434361dce537c8571c090adbf537107
Winstone <90701073+Mhuffy0@users.noreply.github.com>
2026-07-31 15:40:22 +0700

    KIKIKIKI

--- Com-Pro_S1/week2/l2.py
+++ Com-Pro_S1/week2/l2.py
@@ -5,8 +5,11 @@ num = int(input(f"Enter a 3-digit number : "))
 sq = (num ** 2)
 fl = num * 0.25
 bi = num & 0xFF
-temp = int(bi)
-res = f"{temp:08b}"
+res = f"{int(bi):08b}"
+
+unit = num % 10 
+ten = (num // 10) % 10 
+hundred = num // 100 
 
 #Output
 arr = "=>"
@@ -14,7 +17,7 @@ arr = "=>"
 print(f"\nYou have entered {arr:>6} {num}")
 print(f"Square {arr:>16} {sq:,}")
 print(f"25% 3 decimal places {arr:>1} {fl:.3f}%")
-print(f"Flipping {arr:>14} {str(num)[::-1]}")
+print(f"Flipping {arr:>14} {unit * 100 + ten * 10 + hundred}")
 print(f"Hexadecimal {arr:>11} {num:0x} or {num:0X}")
 print(f"Binary {arr:>16} {num:0b}")
-print(f"Binary right 8-digit {arr:>1} {res[:4]} {res[4:]}")
+print(f"Binary right 8-digit {arr:>1} {bi // 16:04b} {bi % 16:04b}")
